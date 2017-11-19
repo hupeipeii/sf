508130a07439d2ea373282ac9dc90df8bff2d89e
hupeipeii <507734042@qq.com>
2017-11-19 19:53:28 +0800

    2

---
+++ 加号的顺序区别.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 21:51:27 2017
+
+@author: hupeipei8090
+"""
+
+x=0
+for i in range(10):
+    x=x+1
+print x
+
+x=0
+for i in range(10):
+    x+=1
+print x
+    
+    
---
+++ 矩阵的输入与输出.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 14:00:39 2017
+
+@author: hupeipei8090
+"""
+
+matrix=[]
+for i in range(2):
+    tmp=input().split('')
+    tmp=[int(x) for x in tmp ]
+    matrix.append(tmp)
+    
+
+    
+    
+    
